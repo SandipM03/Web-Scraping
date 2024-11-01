3cc4ff06371d93d40dd38fd3b9ef97e592fa2c98
SandipM03 <sandipmandal816708@gmail.com>
2024-11-02 01:27:03 +0530

    Implement core feature

--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
 const puppeteer = require('puppeteer')
+import { Browser } from 'puppeteer'
+const url = 'https://www.codesprint.ninja/'
 
+const main = async()=>{
+    const browser:Browser = await puppeteer.launch()
+    
+}
+
+main()
